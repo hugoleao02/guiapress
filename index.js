c0657993cc55348bc09350a0db31f4d4818376a3
Hugo Ferreira Da Silva <81571386+hugoleao02@users.noreply.github.com>
2022-02-16 19:31:21 -0300

    BLog aula

---
+++ index.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const app = express();
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+
+//Controllers
+const categoriesController = require("./categories/categoriesController");
+const articlesCOntroller = require("./articles/articlesController");
+const usersController = require("./users/usersController");
+
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
+const User = require("./users/User");
+
+
+//view engine
+app.set("view engine", "ejs");
+
+//sessions
+app.use(session({
+      secret: "qualquercoisa", cookie: {maxAge: 3000000}//3000 milisegundos
+}));
+
+//static
+app.use(express.static("public"));
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Database
+connection
+        .authenticate()
+        .then(() => {
+        console.log("Conexão feita com sucesso!");
+        })
+        .catch((error) => {
+        console.log(error);
+        });
+
+//Rotas
+app.use("/", categoriesController);
+app.use("/", articlesCOntroller);
+app.use("/", usersController);
+
+
+
+
+app.get("/", (req, res) => {
+      Article.findAll({
+            order: [
+                  ['id', 'DESC']
+            ],
+            limit: 4
+      }).then(articles => {
+            Category.findAll().then(categories => {
+                  res.render("index", {articles: articles, categories: categories});   
+            })   
+      });
+         
+});
+
+app.get("/:slug", (req, res) => {
+      var slug = req.params.slug;
+      Article.findOne({
+            where: {
+                  slug: slug
+            }
+      }).then(article => {
+            if (article != undefined) {
+                  Category.findAll().then(categories => {
+                        res.render("article", { article: article, categories: categories });
+                  });   
+            } else {
+                  res.redirect("/");
+            }
+      }).catch(err => {
+            res.redirect("/");
+      });
+});
+
+app.get("/category/:slug", (req, res) => {
+      var slug = req.params.slug;
+      Category.findOne({
+            where: {
+                  slug: slug
+            },
+            include: [{model:Article, required: true}]
+      }).then(category => {
+            if (category != undefined) {
+                  
+                  Category.findAll().then(categories => {
+                        res.render("index", { articles: category.articles, categories: categories });
+                  });
+            } else {
+                  res.redirect("/"); 
+            }
+      }).catch(err => {
+            res.redirect("/");
+      });
+});
+
+
+app.listen(8080, () => {
+         console.log("Servidor rodando!!!");
+});
